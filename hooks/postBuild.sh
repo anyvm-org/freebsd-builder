0201f8a75b31397c5ce97f603b5cf555dd7dcee8
neil <gitpc@neilpang.com>
2025-11-29 13:47:28 +0100

    init

---
+++ hooks/postBuild.sh
@@ -0,0 +1,25 @@
+#some tasks run in the VM as soon as the vm is up
+
+
+
+
+
+
+echo '=================== start ===='
+
+
+
+
+
+service ntpd enable
+
+service ntpd start
+
+kldload  fusefs
+
+
+
+
+
+
+
---
+++ hooks/waitForLoginTag.sh
@@ -0,0 +1,13 @@
+
+
+
+if [ "$VM_ARCH" = "riscv64" ]; then
+  #for riscv64
+  waitForText "7. Boot Options" 20
+  sleep  20
+  inputKeys "enter"
+fi
+
+
+waitForText "$VM_LOGIN_TAG"
+
