e97a3ee8cf61ef08baccfe44a97a3efd22345a04
neil <gitpc@neilpang.com>
2025-12-01 20:13:24 +0100

    fix for 14.3

--- hooks/postBuild.sh
+++ hooks/postBuild.sh
@@ -44,14 +44,9 @@ sysrc zfs_enable="YES"
 sysrc syslogd_enable="YES"
 sysrc cron_enable="YES"
 
-# SSH
-sysrc sshd_enable="YES"
-sysrc sshd_flags="-o UseDNS=no -o PermitRootLogin=yes"
 
-# Network optimization (use SYNCDHCP; disable background DHCP)
-interface=$(ifconfig -l | awk '{print $1}')
-sysrc ifconfig_${interface}="SYNCDHCP"
-sysrc background_dhcp="NO"
+
+
 
 # Disable unnecessary services
 sysrc growfs_enable="NO"
@@ -60,9 +55,7 @@ sysrc kldxref_enable="NO"
 sysrc zvol_enable="NO"
 sysrc zpoolupgrade_enable="NO"
 sysrc zpoolreguid_enable="NO"
-sysrc devmatch_enable="NO"
-sysrc ip6addrctl_enable="NO"
-sysrc ntpd_enable="NO"
+
 sysrc mixer_enable="NO"
 sysrc rctl_enable="NO"
 sysrc virecover_enable="NO"
