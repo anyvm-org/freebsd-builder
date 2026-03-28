114a639d21252da3e55cfc42378acf97ec17c2ad
neil <gitpc@neilpang.com>
2026-03-28 19:48:16 +0800

    revert

--- hooks/finalize.sh
+++ hooks/finalize.sh
@@ -6,23 +6,3 @@ sed -E -i '' 's|ttyv0[[:space:]]+"/usr/libexec/getty Pc"|ttyv0	  "/usr/libexec/g
 
 
 
-# Zero unused disk space on filesystems that have had activity.
-for fs in / /usr /var /tmp; do
-  echo zeroing unused space on $fs
-  dd if=/dev/zero of=$fs/zero bs=1m >/dev/null 2>&1 || true
-  sync; sync; sync
-  rm -f $fs/zero
-done
-# Clear swap space
-swap=$(swapinfo | awk 'NR>1 {print $1}')
-if [ ! -z "$swap" ]; then
-  for s in $swap; do
-    echo zeroing swap $s
-    swapoff $s
-    dd if=/dev/zero of=$s bs=1m >/dev/null 2>&1
-    swapon $s
-  done
-fi
-
-
-
